9627fe1ac199381e4cb253c91e065209d81a8ff2
xiaohaoyong <xiaohaoyong@126.com>
2017-06-12 11:44:14 +0800

    12

--- js/article.js
+++ js/article.js
@@ -24,20 +24,55 @@ $(function(){
     //关注 已关注
     $('.js-favor-btn').click(function(event) {
         var dataId = $(this).attr('data-id');
+        var userid = getCookie('Yimai-Request');
         if($(this).hasClass('on')){
-            $.post('http://api.app.wedoctors.com.cn/1.5/index.interface.php?a=friends&m=delfollow', { touserid: dataId},function(e){
-                if(e.code=='10000')
+            $.post('http://api.app.wedoctors.com.cn/1.5/index.interface.php?a=friends&m=delfollow', { touserid: dataId,'Yimai-Request':userid},function(e){
+                if(e.code==10000)
                 {
                     $('.favor_Btn').removeClass('on').html('+ 关注');//取消关注
+                }else{
+                    alert(e.msg);
                 }
             },'json');
         }else{
-            $.post('http://api.app.wedoctors.com.cn/1.5/index.interface.php?a=friends&m=addfollow', { touserid: dataId},function(e){
-                if(e.code=='10000')
+            $.post('http://api.app.wedoctors.com.cn/1.5/index.interface.php?a=friends&m=addfollow', { touserid: dataId,'Yimai-Request':userid},function(e){
+                if(e.code==10000)
                 {
                     $('.favor_Btn').addClass('on').html('已关注');
+                }else{
+                    alert(e.msg);
                 }
             },'json');
         }
     });
+    $('.article_praise').click(function(event) {
+        var dataId = $(this).attr('data-id');
+        var userid = getCookie('Yimai-Request');
+        var article_praise = $(this);
+        var num = $(this).html()
+        if($(this).hasClass('over')){
+            alert("您已经点赞！");
+        }else{
+            $.post('http://api.app.wedoctors.com.cn/1.5/zixun/index.interface.php?a=actionAdd&c=praise', { type:2,id:dataId,'Yimai-Request':userid},function(e){
+                if(e.code===0)
+                {
+                    num = parseInt(num)+1;
+                    article_praise.addClass('over');
+                    article_praise.html(num);
+                }else{
+                    alert(e.msg);
+                }
+            },'json');
+
+        }
+    });
 })
+
+function getCookie(name)
+{
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
