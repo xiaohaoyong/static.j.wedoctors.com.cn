cda7f594ca1691046e92f1082b0c37421fb09837
xiaohaoyong <xiaohaoyong@126.com>
2017-06-12 14:22:14 +0800

    12

--- js/article.js
+++ js/article.js
@@ -11,9 +11,11 @@ $(function(){
         var h = $('.Infor_Th').height();
         if(scrollTop > h){
             //触发
+            setupWebViewJavascriptBridge('showMediaView');
             console.log(1)
         }else{
             //消失
+            setupWebViewJavascriptBridge('dismissMediaView');
             console.log(0)
         }
 
@@ -85,4 +87,14 @@ function getCookie(name)
         return unescape(arr[2]);
     else
         return null;
+}
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
+    if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'https://__bridge_loaded__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function() { document.documentElement.removeChild(WVJBIframe) }, 0)
 }
