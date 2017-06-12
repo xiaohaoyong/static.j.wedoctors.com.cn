caffb380526dcc7be2171ef53973b3c8eec02453
xiaohaoyong <xiaohaoyong@126.com>
2017-06-12 15:22:51 +0800

    12

--- js/article.js
+++ js/article.js
@@ -11,12 +11,10 @@ $(function(){
         var h = $('.Infor_Th').height();
         if(scrollTop > h){
             //触发
-            alert('showMediaView');
             setupWebViewJavascriptBridge('showMediaView');
             console.log(1)
         }else{
             //消失
-            alert('dismissMediaView');
 
             setupWebViewJavascriptBridge('dismissMediaView');
             console.log(0)
